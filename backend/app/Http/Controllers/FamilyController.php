e536c6cd26d8f020762cfce2ec95e110e3298403
VesztegBernadett <vdetti2004@gmail.com>
2024-10-21 12:32:07 +0000

    óra vége

---
+++ backend/app/Http/Controllers/FamilyController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\View\View;
+
+class FamilyController extends Controller
+{
+    function index():View{
+        return view("families.index",[
+            "title"=>"Családok",
+            "families"=>$this->loadFamilies()
+        ]);
+    }
+    function table():View{
+        return view("families.table",[
+            "title"=>"Karakterek",
+            "families"=>$this->loadCharacters()
+        ]);
+    }
+    function loadFamilies(){
+        $families = [];
+        foreach ($this->loadCharacters() as $key => $value) {
+            $families[$key]=$value[0]["family"];
+        }
+        return $families;
+    }
+    function show(Request $request) : View {
+        $slug = $request->query("slug");
+        $id = $request->query("id");
+        $character = $this->loadCharacters()[$slug][$id];
+        return view("families.show",[
+            "title"=>"Karakter",
+            "character"=>$character,
+            "slug"=>$slug
+        ]);
+    }
+    function list(Request $request):View{
+        $slug = $request->query("slug");
+        $characters = $this->loadCharacters()[$slug];
+        return view("families");
+    }
+    function loadCharacters():Collection{
+        return collect(
+            [
+                "simpson" => [
+                    [
+                        "family"=> "A Simpson család",
+                        "year"=> 1987,
+                        "character"=> "Homer Simpson",                        
+                        "image"=> "homer.png",
+                        "color"=> "#ffef00"
+                    ],
+                    [
+                        "family"=> "A Simpson család",
+                        "year"=> 1987,
+                        "character"=> "Bart Simpson",                        
+                        "image"=> "bart.png",                        
+                        "color"=> "#ffef00"
+                    ],
+                    [
+                        "family"=> "A Simpson család",
+                        "year"=> 1987,
+                        "character"=> "Marge Simpson",                        
+                        "image"=> "marge.png",                        
+                        "color"=> "#ffef00"
+                    ],
+                    [
+                        "family"=> "A Simpson család",
+                        "year"=> 1987,
+                        "character"=> "Lisa Simpson",
+                        "image"=> "lisa.png",                 
+                        "color"=> "#ffef00"
+                    ],
+                ],
+                "mezga" => [
+                    [
+                        "family"=> "A Mézga család",
+                        "year"=> 1968,
+                        "character"=> "Mézga Géza",                        
+                        "image"=> "mezgageza.jpg",                        
+                       "color"=> "#bd7481"
+                    ],
+                    [
+                        "family"=> "A Mézga család",
+                        "year"=> 1968,
+                        "character"=> "Mézga Paula",                        
+                        "image"=> "mezgapaula.jpg",                        
+                       "color"=> "#bd7481"
+                    ],
+                    [
+                        "family"=> "A Mézga család",
+                        "year"=> 1968,
+                        "character"=> "Mézga Kriszta",                        
+                        "image"=> "mezgakriszta.jpg",                        
+                       "color"=> "#bd7481"
+                    ],
+                    [
+                        "family"=> "A Mézga család",
+                        "year"=> 1968,
+                        "character"=> "Mézga Aladár",                        
+                        "image"=> "mezgaaladar.jpg",                        
+                       "color"=> "#bd7481"
+                    ],
+                    [
+                        "family"=> "A Mézga család",
+                        "year"=> 1968,
+                        "character"=> "MZ/X",
+                        "image"=> "mzx.jpg",
+                       "color"=> "#bd7481"
+                    ],
+                ],
+                "familyguy"=> [
+                    [
+                        "family"=> "Family Guy",
+                        "year"=> 1999,
+                        "character"=> "Peter Griffin",                        
+                        "image"=> "petergriffin.png",                        
+                        "color"=> "#9dc4ea"
+                    ],
+                    [
+                        "family"=> "Family Guy",
+                        "year"=> 1999,
+                        "character"=> "Lois Griffin",                        
+                        "image"=> "loisgriffin.png",                        
+                        "color"=> "#9dc4ea"
+                    ],
+                    [
+                        "family"=> "Family Guy",
+                        "year"=> 1999,
+                        "character"=> "Chris Griffin",                        
+                        "image"=> "chrisgriffin.png",                        
+                        "color"=> "#9dc4ea"
+                    ],
+                    [
+                        "family"=> "Family Guy",
+                        "year"=> 1999,
+                        "character"=> "Meg Griffin",                        
+                        "image"=> "meggriffin.png",                        
+                        "color"=> "#9dc4ea"
+                    ],
+                    [
+                        "family"=> "Family Guy",
+                        "year"=> 1999,
+                        "character"=> "Stewie Griffin",
+                        "image"=> "stewiegriffin.png",
+                        "color"=> "#9dc4ea"
+                    ],
+                ]
+            ]
+        );
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\FamilyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[FamilyController::class,"index"])->name("home");
+Route::get('/families/characters',[FamilyController::class,"table"])->name("families.table");
+Route::get('/families/{slug}/{id}',[FamilyController::class,"table"])->name("families.table");
+Route::get('/families/{slug}',[FamilyController::class,"list"])->name("families.list");
+
