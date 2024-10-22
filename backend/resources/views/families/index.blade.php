0a337c289dc0662544072841ee08a725bd7f40bd
VesztegBernadett <vdetti2004@gmail.com>
2024-10-22 10:29:59 +0000

    layouts, index view elkészítése, képek hozzáadása

--- backend/resources/views/families/index.blade.php
+++ backend/resources/views/families/index.blade.php
@@ -1,1 +1,23 @@
-index.blade.php
+@extends('layouts.main')
+
+@section("title",$title)
+
+@section("content")
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center">{{$title}}</h1>
+        </div>
+        <div class="col-12">
+            <ul>
+                @foreach($families as $key => $value)
+                @php $url = route('families.list', ['slug' => $key]) @endphp
+                <li><a href="{{$url}}">{{$value}}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</div>
+
+@endsection
